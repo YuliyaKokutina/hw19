034b364106df58dece201f83b9ff3e8a97bd317e
Yulya <Kokutinayulya2001@gmail.com>
2023-09-19 22:40:54 +0200

    Решила задачи

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+int count = 0;
+int [] Convert(string message)
+{
+System.Console.WriteLine($"{message}");
+string stroka = Console.ReadLine();
+string [] str = stroka.Split(", ");
+int [] arr = new int[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    arr[i]=int.Parse(str[i]);
+}
+return arr;
+}
+int [] array = Convert("Введите числа");
+
+int Totalizer(int [] array)
+{
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]>0)
+    {count++;}
+}
+return count;
+}
+count = Totalizer(array);
+System.Console.WriteLine(count);
---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+// int [] Convert(string message)
+
+System.Console.WriteLine("Введите коэффициенты");
+string stroka = Console.ReadLine();
+string [] str = stroka.Split(", ");
+double b1 = double.Parse(str[0]);
+double k1 = double.Parse(str[1]);
+double b2 = double.Parse(str[2]);
+double k2 = double.Parse(str[3]);
+if(k1-k2==0)
+{
+    System.Console.WriteLine("Прямые не пересекутся");
+}
+else {
+double x0 = Math.Round((b2-b1)/(k1-k2),2);
+double y0 = Math.Round(k1*x0+b1,2);
+System.Console.WriteLine($"Точка пересечения {x0}; {y0}"); }
